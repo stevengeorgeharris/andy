df7dfb5944710a835ee6b9942768a4e3ae04a0ee
stevengeorgeharris <mail@stevengeorgeharris.com>
2016-02-20 22:58:32 +0000

    Started menu/brand

--- themes/andydonohoe/index.php
+++ themes/andydonohoe/index.php
@@ -1,18 +1,6 @@
 <?php
-  /**
-   * Contains head references.
-   *
-   * @package nebula
-   */
+  get_header();
+
+  
+  get_footer();
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
-
-<body>
---
+++ themes/andydonohoe/template-parts/brand-menu.php
@@ -0,0 +1,14 @@
+<div class="header">
+  <div class="header__brand">
+    <span class="brand-main">Andy Donohoe</span>
+    <span class="brand-sub">Photographer</span>
+  </div>
+
+  <div class="header__menu-button">
+    <!-- Place button here. -->
+  </div>
+</div>
+
+<div class="menu menu--hidden">
+  <!-- Pop out menu -->
+</div>

1a75a8456013240c54e4ef4ccc772fdc07a252ec
stevengeorgeharris <mail@stevengeorgeharris.com>
2016-05-07 13:47:33 +0100

    Latest

--- themes/andydonohoe/template-parts/projects.php
+++ themes/andydonohoe/template-parts/projects.php
@@ -2,7 +2,6 @@
 <div class="projects__measure"></div>
 
 <?php
-  // TODO:: Split into 3 columns > Click opens slideshow.
 
   $args = array(
     'post_type' => 'projects',
@@ -10,11 +9,11 @@
   $query = new WP_Query($args);
 
   while ( $query -> have_posts() ) : $query -> the_post();
+  $link = get_permalink();
+  $title = get_the_title();
+  $image = get_the_post_thumbnail();
     echo '<div class="projects__card">';
-    $link = get_permalink();
-    $title = get_the_title();
-    $image = get_the_post_thumbnail();
-    echo $image;
+    echo '<a href="' . $link . '">' . $image . '</a>';
     echo '<a href="' . $link . '">' . $title . '</a>';
     echo '</div>';
   endwhile;
